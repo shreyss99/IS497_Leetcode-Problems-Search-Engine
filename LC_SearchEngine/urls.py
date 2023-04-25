cfdec1b823608e6d4acdf527ff8a0c6599e3c740
Shrey Shailesh Shah <shreyss99@gmail.com>
2023-04-25 03:05:30 -0500

    Updated urls and views

--- LC_SearchEngine/urls.py
+++ LC_SearchEngine/urls.py
@@ -5,5 +5,6 @@ from LC_SearchEngine import views
 urlpatterns = [
     path("", views.home, name="LC_SearchEngine_Home"),
     path("about/", views.about_us, name="LC_SearchEngine_About"),
+    path("data/", views.data_view, name="LC_SearchEngine_Data"),
     path("data/", views.data_view, name="LC_SearchEngine_Data")
 ]
--- LC_SearchEngine/views.py
+++ LC_SearchEngine/views.py
@@ -1,54 +1,25 @@
 from django.shortcuts import render
 from LC_SearchEngine.database import get_data
-
-leetcode_problems = [
-    {
-        "problem_id": 1,
-        "title": "Two Sum",
-        "description": "Given an array of integers nums and an integer target, return indices of the two numbers such "
-                       "that they add up to target. You may assume that each input would have exactly one solution, "
-                       "and you may not use the same element twice. You can return the answer in any order.",
-        "is_premium": 0,
-        "difficulty": "easy",
-        "solution_link": "/articles/two-sum",
-        "acceptance_rate": 46.7,
-        "frequency": 100.0,
-        "url": "https://leetcode.com/problems/two-sum",
-        "discuss_count": 999,
-        "accepted": "4.1M",
-        "submissions": "8.7M",
-        "companies": "Amazon,Google,Apple,Adobe,Microsoft,Bloomberg,Facebook,Oracle,Uber,Expedia,Twitter,Nagarro,SAP,"
-                     "Yahoo...",
-        "related_topics": "Array,Hash Table",
-        "likes": 20217,
-        "dislikes": 712,
-        "rating": 97,
-        "asked_by_faang": 1,
-        "similar_questions": "[3Sum, /problems/3sum/, Medium], [4Sum, /problems/4sum/, Medium], [Two Sum II - Input "
-                             "array is sorte..."
-    }
-]
+from LC_SearchEngine.forms import QueryForm
+from LC_SearchEngine.models import LeetcodeData
 
 
 def home(request):
-    context = {
-        "lc_problem_details": leetcode_problems
-    }
-    print(context)
-    return render(request, "LC_SearchEngine/home.html", context)
+    return render(request, "LC_SearchEngine/home.html")
 
 
 def about_us(request):
     return render(request, "LC_SearchEngine/about_us.html")
 
 
-def data_view(request):
-    lc = get_data()
-    for item in lc:
-        print(item[1])
-    context = {
-        "lc_problem_details": leetcode_problems
-    }
-    #print(context)
-    return render(request, "LC_SearchEngine/data.html", context)
+def display_query_form(request):
+    form = QueryForm()
+    context = {'form': form}
+    return render(request, "LC_SearchEngine/forms.html", context)
 
+
+def data_view(request):
+
+    fetched_data = get_data()
+    context = {"fetched_lc_data": {obj.id: obj for obj in fetched_data}}
+    return render(request, "LC_SearchEngine/data.html", context)
