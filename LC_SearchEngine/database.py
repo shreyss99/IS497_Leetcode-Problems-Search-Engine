7fe83e1299991eac4f231de0c24d7d11036064ed
Shrey Shailesh Shah <shreyss99@gmail.com>
2023-04-25 03:04:21 -0500

    Updated data fetch method to resolve table display issue

--- LC_SearchEngine/database.py
+++ LC_SearchEngine/database.py
@@ -1,8 +1,18 @@
-from LC_SearchEngine.models import LeetcodeData
 from django.db import connection
+from .models import LeetcodeData
+
 
 def get_data():
-    with connection.cursor() as cursor:
-        cursor.execute("""SELECT * FROM "LC_SearchEngine_leetcodedata" where difficulty = 'Easy'""")
-        rows = cursor.fetchall()
-    return rows
+    # with connection.cursor() as cursor:
+    #     cursor.execute(""" SELECT id, title, url FROM "LC_SearchEngine_leetcodedata" """)
+    #     rows = cursor.fetchall()
+    # return rows
+
+    """ TEMP QUERY
+    Need to make use of user parameters
+    """
+    data = LeetcodeData.objects.filter(title__regex=r'',
+                                       is_premium=0,
+                                       acceptance_rate__gte=37,
+                                       difficulty__regex=r'').all()
+    return data
