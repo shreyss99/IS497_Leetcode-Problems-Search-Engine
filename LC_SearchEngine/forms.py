c6cffe86086e47fc97ded2b956308c5e159503a7
Shrey Shailesh Shah <shreyss99@gmail.com>
2023-04-30 15:47:12 -0500

    Added code for contributor

--- LC_SearchEngine/forms.py
+++ LC_SearchEngine/forms.py
@@ -7,12 +7,41 @@ class QueryForm(forms.Form):
     IS_PREMIUM_OPTIONS = [(True, 'Yes'), (False, 'No')]
     ASKED_BY_FAANG_OPTIONS = [(True, 'Yes'), (False, 'No')]
 
-    title = forms.CharField(label='Title', max_length=100, required=True)
-    is_premium = forms.ChoiceField(label='Is Premium Question?', choices=IS_PREMIUM_OPTIONS,
+    is_premium = forms.ChoiceField(label='Is Premium?', choices=IS_PREMIUM_OPTIONS,
                                    widget=forms.RadioSelect, required=True)
+    title = forms.CharField(label='Title', max_length=100, required=True)
+
     difficulty = forms.ChoiceField(label='Difficulty Level', choices=DIFFICULTY_OPTIONS,
                                    widget=forms.RadioSelect, required=True)
     companies = forms.CharField(max_length=100, required=True)
     related_topics = forms.CharField(max_length=100, required=True)
     asked_by_faang = forms.ChoiceField(label='Asked By FAANG?', choices=ASKED_BY_FAANG_OPTIONS,
                                        widget=forms.RadioSelect, required=True)
+
+
+class ProblemSubmitForm(forms.Form):
+
+    DIFFICULTY_OPTIONS = [('Easy', 'Easy'), ('Medium', 'Medium'), ('Hard', 'Hard')]
+    IS_PREMIUM_OPTIONS = [(True, 'Yes'), (False, 'No')]
+    ASKED_BY_FAANG_OPTIONS = [(True, 'Yes'), (False, 'No')]
+
+    title = forms.CharField(label='Title', max_length=100, required=True)
+    description = forms.CharField(widget=forms.TextInput, required=True)
+    is_premium = forms.ChoiceField(label='Is Premium?', choices=IS_PREMIUM_OPTIONS,
+                                   widget=forms.RadioSelect, required=True)
+    difficulty = forms.ChoiceField(label='Difficulty Level', choices=DIFFICULTY_OPTIONS,
+                                   widget=forms.RadioSelect, required=True)
+    solution_link = forms.CharField(label='Solution Link', max_length=200, required=False)
+    acceptance_rate = forms.FloatField(label='Acceptance Rate', required=True)
+    frequency = forms.FloatField(label='Frequency', required=True)
+    url = forms.CharField(label='Solution URL', max_length=200, required=True)
+    discuss_count = forms.IntegerField(label='Discuss Count', required=True)
+    accepted = forms.CharField(label='Accepted', max_length=10, required=True)
+    submissions = forms.CharField(label='Submissions', max_length=10, required=True)
+    companies = forms.CharField(label='Companies', max_length=100, required=True)
+    related_topics = forms.CharField(label='Related Topics', max_length=300, required=False)
+    likes = forms.IntegerField(label='Likes', required=True)
+    dislikes = forms.IntegerField(label='Dislikes', required=True)
+    asked_by_faang = forms.ChoiceField(label='Asked By FAANG?', choices=ASKED_BY_FAANG_OPTIONS,
+                                       widget=forms.RadioSelect, required=True)
+    similar_questions = forms.CharField(label='Similar Questions', widget=forms.TextInput, required=False)
--- LC_SearchEngine/views.py
+++ LC_SearchEngine/views.py
@@ -1,10 +1,7 @@
 from django.contrib import messages
 from django.db import connection
-from django.db.models import Q
 from django.shortcuts import render, redirect
-from LC_SearchEngine.database import get_data
-from LC_SearchEngine.forms import QueryForm
-from LC_SearchEngine.models import LeetcodeData
+from LC_SearchEngine.forms import QueryForm, ProblemSubmitForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -17,33 +14,30 @@ def about_us(request):
 
 
 @login_required()
-def data_view(request):
-
-    fetched_data = get_data()
-    context = {"fetched_lc_data": {obj.id: obj for obj in fetched_data}}
-    return render(request, "LC_SearchEngine/data.html", context)
-
-
-@login_required()
 def query_and_display(request):
 
-    form = QueryForm()
+    q_form = QueryForm()
 
     if request.method == 'POST':
-        form = QueryForm(request.POST)
+        q_form = QueryForm(request.POST)
 
-        if form.is_valid():
-            title = form.cleaned_data.get('title')
-            is_premium = form.cleaned_data.get('is_premium')
-            difficulty = form.cleaned_data.get('difficulty')
-            companies = form.cleaned_data.get('companies')
-            related_topics = form.cleaned_data.get('related_topics')
-            asked_by_faang = form.cleaned_data.get('asked_by_faang')
+        if q_form.is_valid():
+            title = q_form.cleaned_data.get('title')
+            is_premium = q_form.cleaned_data.get('is_premium')
+            difficulty = q_form.cleaned_data.get('difficulty')
+            companies = q_form.cleaned_data.get('companies')
+            related_topics = q_form.cleaned_data.get('related_topics')
+            asked_by_faang = q_form.cleaned_data.get('asked_by_faang')
 
             with connection.cursor() as cursor:
-                cursor.execute(f""" SELECT id, title, url FROM "LC_SearchEngine_leetcodedata" WHERE 
+                if (not title) or (not difficulty) or (not companies) or (not related_topics) or (not asked_by_faang):
+                    cursor.execute(f""" SELECT id, title, url FROM "LC_SearchEngine_leetcodedata" WHERE 
+                                                        is_premium = {is_premium} """)
+
+                else:
+                    cursor.execute(f""" SELECT id, title, url FROM "LC_SearchEngine_leetcodedata" WHERE 
+                                    is_premium = {is_premium} AND 
                                     title LIKE '%{title}%' AND
-                                    is_premium = {is_premium} AND
                                     difficulty = '{difficulty}' AND
                                     companies LIKE '%{companies}%' AND
                                     related_topics LIKE '%{related_topics}%' AND
@@ -51,8 +45,71 @@ def query_and_display(request):
 
                 fetched_lc_data = cursor.fetchall()
 
-            context = {"form": form, "fetched_lc_data": fetched_lc_data}
+            context = {"form": q_form, "fetched_lc_data": fetched_lc_data}
             return render(request, "LC_SearchEngine/data.html", context)
 
-    context = {"form": form}
-    return render(request, "LC_SearchEngine/forms.html", context)
+    context = {"form": q_form}
+    return render(request, "LC_SearchEngine/query_form.html", context)
+
+
+@login_required()
+def contribute(request):
+
+    user = request.user
+    username = user.username
+    role = user.person.role
+
+    ps_form = ProblemSubmitForm()
+
+    if request.method == 'POST':
+        if role == "contributor":
+
+            ps_form = ProblemSubmitForm(request.POST)
+
+            if ps_form.is_valid():
+                title = ps_form.cleaned_data.get('title')
+                description = ps_form.cleaned_data.get('description')
+                is_premium = ps_form.cleaned_data.get('is_premium')
+                difficulty = ps_form.cleaned_data.get('difficulty')
+                solution_link = ps_form.cleaned_data.get('solution_link')
+                acceptance_rate = ps_form.cleaned_data.get('acceptance_rate')
+                frequency = ps_form.cleaned_data.get('frequency')
+                url = ps_form.cleaned_data.get('url')
+                discuss_count = ps_form.cleaned_data.get('discuss_count')
+                accepted = ps_form.cleaned_data.get('accepted')
+                submissions = ps_form.cleaned_data.get('submissions')
+                companies = ps_form.cleaned_data.get('companies')
+                related_topics = ps_form.cleaned_data.get('related_topics')
+                likes = ps_form.cleaned_data.get('likes')
+                dislikes = ps_form.cleaned_data.get('dislikes')
+                rating = round((likes / (likes + dislikes) * 100))
+                asked_by_faang = ps_form.cleaned_data.get('asked_by_faang')
+                similar_questions = ps_form.cleaned_data.get('similar_questions')
+
+                with connection.cursor() as cursor:
+                    cursor.execute(""" SELECT MAX(id) FROM "LC_SearchEngine_leetcodedata" """)
+                    rows = cursor.fetchall()
+
+                    for row in rows:
+                        max_id = int(row[0])
+
+                    cursor.execute(f""" INSERT INTO "LC_SearchEngine_leetcodedata" (id, title, description, 
+                    is_premium, difficulty, solution_link, acceptance_rate, frequency, url, discuss_count, accepted, 
+                    submissions, companies, related_topics, likes, dislikes, rating, asked_by_faang, similar_questions) 
+                    VALUES ({max_id+1}, '{title}', '{description}', {is_premium}, '{difficulty}', '{solution_link}', 
+                    {acceptance_rate}, {frequency}, '{url}', {discuss_count}, '{accepted}', '{submissions}', '{companies}',
+                    '{related_topics}', {likes}, {dislikes}, {rating}, {asked_by_faang}, '{similar_questions}') """)
+
+                context = {"ps_form": ps_form}
+                return render(request, "users/thanks.html", context)
+
+        else:
+            messages.warning(request, f"{username} does not have the privileges of a contributor")
+            return render(request, "LC_SearchEngine/home.html")
+
+    context = {"ps_form": ps_form}
+    return render(request, "users/contributor.html", context)
+
+
+def thank(request):
+    return render(request, "users/thanks.html")
